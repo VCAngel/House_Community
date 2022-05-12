54541d0af4f3296b585ad09bfae238e628de9262
VCAngel <vargascasavantesangel.329@gmail.com>
2022-05-12 01:43:38 -0600

    Basic login page done; general styling pending

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
+  Heading,
   Text,
   Link,
-  VStack,
-  Code,
-  Grid,
+  List,
+  ListItem,
+  FormControl,
+  FormLabel,
+  Input,
+  Divider,
+  Button,
   extendTheme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import bgImage from './img/header.jpg';
 
-const base = {
+const colors = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
@@ -21,30 +27,57 @@ const base = {
 }
 
 //! Webpage theme
-const theme = extendTheme({base}) 
+const theme = extendTheme({ colors })
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+      {/* TODO Create routes for each link */}
+      <Box display="flex" flexDirection="column" h="100vh" bgImage={`url(${bgImage})`} bgPosition="center" bgRepeat="no-repeat">
+        <header>
+
+          <Box mt="1.2rem" mb="1.2rem" display="flex" alignItems="center" justifyContent="space-around">
+            <Link href="#">House's Community</Link>
+            <List display="flex" alignItems="center" justifyContent="space-between" gap="1rem">
+              <ListItem>
+                <Link href="#">Anuncios</Link>
+              </ListItem>
+              <Link href="#">Pagos</Link>
+              <ListItem></ListItem>
+              <Link href="#">Directorio</Link>
+              <ListItem></ListItem>
+              <Link href="#">Contacto</Link>
+              <ListItem></ListItem>
+            </List>
+          </Box>
+        </header>
+
+        <Box display="flex" flex={1} alignItems="center" justifyContent="center">
+          <Box display="grid" w="100%" gridTemplateColumns="repeat(2,1fr)">
+            <Heading m="auto auto" w="50%" size="2xl" overflowWrap="normal" whiteSpace="normal">House's Community</Heading>
+            <Box marginY={0} marginX="auto" p="2rem 3rem" bgColor="white" border="1px solid gray" borderRadius="1%">
+              <FormControl display="flex" flexDirection="column" gap="1rem">
+                <div>
+                  <FormLabel htmlFor='email'>Correo Electrónico</FormLabel>
+                  <Input id='email' type='email' />
+                </div>
+                <div>
+                  <FormLabel htmlFor='password'>Contraseña</FormLabel>
+                  <Input id='pasword' type='password' />
+                </div>
+                <Button w="100%" colorScheme="green">Iniciar Sesión</Button>
+              </FormControl>
+
+              <Divider mt="1rem" marginX="auto" />
+              <Text align="center">
+                ¿No tienes cuenta?{' '}
+                <Link color="green" href="#">
+                  Contáctanos
+                </Link>
+              </Text>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </ChakraProvider>
   );
